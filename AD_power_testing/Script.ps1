42f7315e1302153f918371cbf437ddc6a5408d45
George <George@FAILING>
2017-03-05 17:00:28 -0600

    working

--- AD_power_testing/Script.ps1
+++ AD_power_testing/Script.ps1
@@ -1,16 +1,36 @@
 ﻿#
-# Script.ps1
+# User Copy .ps1
 #
-$template = get-aduser -identity "CN=Food Nutrician,OU=Food and Nutrition,OU=Departments,DC=WHPHDOM,DC=local" -properties company,department,description,Manager,MemberOf,Organization,homeDrive 
+$template = get-aduser `
+    -identity "CN=User Copy,CN=testUserOU,CN=Users,DC=adlabdom,DC=local" `
+    -properties company,department,description,MemberOf,Organization 
 
-Import-Csv .\usercreationfile.csv | foreach-object { 
-$userprinicpalname = $_.SamAccountName + “@whphdom.local” 
+Import-Csv .\Documents\usrecreationfile.csv | foreach-object { 
+$userprinicpalname = $_.SamAccountName + “@adlabdom.local” 
 $group = $_.memberOf 
-$oubits =  "OU=Food and Nutrition,OU=Departments,DC=WHPHDOM,DC=local"
+$oubits =  "CN=testUserOU,CN=Users,DC=adlabdom,DC=local"
 $addMember = ("Cn=" + $_.name + "," + $oubits)
-$homeDirectory = "\\svrwhph02\Users\" + $_.SamAccountName 
-New-ADUser -SamAccountName $_.SamAccountName -UserPrincipalName $userprinicpalname -Name $_.name -DisplayName $_.name -GivenName $_.cn -SurName $_.sn -Department $_.Department -homeDrive $_.homeDrive -description $_.description -Manager $_.Manager -homeDirectory $homeDirectory -Path “OU=Food and Nutrition,OU=Departments,DC=WHPHDOM,DC=local” -AccountPassword (ConvertTo-SecureString “Microsoft~1;” -AsPlainText -force) -Enabled $True -PasswordNeverExpires $False -PassThru 
+
+New-ADUser -Name $_.name `
+ -AccountPassword (ConvertTo-SecureString “Microsoft~1;” -AsPlainText -force) `
+ -Company $template.Company `
+ -Department $_.Department `
+ -Description $_.description `
+ -DisplayName $_.name `
+ -Enabled $true `
+ -GivenName $_.cn `
+ -PassThru `
+ -Path $oubits `
+ -samAccountName $_.SamAccountName `
+ -Server lab-svr1.adlabdom.local `
+ -Surname $_.sn `
+ -Title $_.Job_title `
+ -UserPrincipalName $userprinicpalname
 
  foreach($group in $template.MemberOf) {
-            $null = Add-ADGroupMember -identity $group -Members $addMember -server svrwhph01.whphdom.local
-        }}
+            $null = Add-ADGroupMember `
+            -identity $group `
+            -Members $addMember `
+            -server lab-svr1.adlabdom.local
+        }
+   }

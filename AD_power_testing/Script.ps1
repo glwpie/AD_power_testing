4280b422c31fd704ce04ffdcb1fbaa7a812e892f
glwpie <georgewhiting1@gmail.com>
2017-05-03 14:35:19 -0500

    Update Script.ps1

--- AD_power_testing/Script.ps1
+++ AD_power_testing/Script.ps1
@@ -1,19 +1,4 @@
-﻿#
-# User Copy .ps1
-# current project = offload duplicate fields in csv
-#set Department Domain Location
-$commaDepDomLoc = ",CN=Users,DC=adlabdom,DC=local"
-# setUser csv location
-$csvloc = ".\Documents\usrecreationfile.csv"
-# Get Company, memberof, orginization of example Profile
-$template = get-aduser `
-    -identity "CN=User Copy,CN=testUserOU" + $commaDepDomLoc `
-    -properties company,MemberOf,Organization 
-
-Import-Csv $csvloc |  foreach-object {
-:confbreak {
-$name = $_.FN_custom + " " + SN
-$SamAccountName = $_.FN_custom + "." + $_.SN#
+#
 # User Copy .ps1
 # check variables for user copy
 #set Department Domain Location
@@ -99,47 +84,4 @@ foreach($group in $template.MemberOf) {
         }
 #email setup here
 #Enable-Mailbox -Identity $name -Alias $SamAccountName
-}   
-$userprinicpalname = $SamAccountName + “@adlabdom.local” 
-$group = $_.memberOf 
-$oubits =  "CN=testUserOU,CN=Users,DC=adlabdom,DC=local"
-$CN = ("Cn=" + $name + "," + $oubits)
-$manager = $_.manager + $oubits
-$checkconf = Get-ADUser `
-    -identity $SamAccountName `
-    -properties UserPrincipalName,distinguishedName
-    
-    If($checkconf.userPrincipalName == $userprincipalname) {
-        cout << "There is a conflict with this AD User:"
-        cout << $checkconf.distinguishedName 
-        break confbreak
-    }
-New-ADUser `
-     -Name $_.name `
-     -AccountPassword (ConvertTo-SecureString “Welcome1” -AsPlainText -force) `
-     -Company $template.Company `
-     -Department $_.Department `
-     -Description $_.description `
-     -DisplayName $namename `
-     -Enabled $true `
-     -GivenName $CN `
-     -PassThru `
-     -Path $oubits `
-     -samAccountName $SamAccountName `
-     -Server lab-svr1.adlabdom.local `
-     -Surname $_.sn `
-     -Title $_.Job_title `
-     -UserPrincipalName $userprinicpalname
-
-foreach($group in $template.MemberOf) {
-            $null = Add-ADGroupMember `
-            -identity $group `
-            -Members $CN `
-            -server lab-svr1.adlabdom.local
-        }
-   #email setup here
-#enable-mailbox -identity $userprincipalname `
-#  -database  [-DisplayName <String>] [-DomainController <Fqdn>]
-
-   }
 }   
